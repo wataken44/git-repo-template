8c6e933bb6d147b71f8daf424ab7e456f07ef19e
wataken44 <wataken44@gmail.com>
2019-05-08 23:13:56 +0900

    initial commit

---
+++ create_repo.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" create_repo.py
+
+
+"""
+
+import os
+import re
+import sys
+
+BASEDIR=os.path.dirname(__file__) + "/"
+
+def main():
+    repo = sys.argv[1]
+
+    user = None
+    if "USER" in os.environ:
+        user = os.environ["USER"]
+    elif "USERNAME" in os.environ:
+        user = os.environ["USERNAME"]
+
+    if len(sys.argv) >= 3:
+        user = sys.argv[2]
+
+    # create repository directory
+    os.mkdir(repo)
+
+    # cd to repo
+    os.chdir(repo)
+
+    # git init
+    os.system("git init")
+
+    # gitignore
+    os.system("curl -Ssl 'https://www.gitignore.io/api/vim,emacs,visualstudiocode' > .gitignore")
+
+    # create docker-compose directory
+    os.mkdir("docker")
+
+    # create docker-compose.yml
+    render(
+        "%s/template/docker-compose.yml" % BASEDIR,
+        "docker/docker-compose.yml", user, repo)
+    
+    # create docker directory
+    os.mkdir("docker/%s" % repo)
+
+    # create Dockerfile
+    render(
+        "%s/template/Dockerfile" % BASEDIR,
+        "docker/%s/Dockerfile" % repo, user, repo)
+
+    # create LICENSE
+    render("%s/template/LICENSE" % BASEDIR, "LICENSE" , user, repo)
+
+    # create README.md
+    render("%s/template/README.md" % BASEDIR, "README.md" , user, repo)
+    
+    # git remote add
+    os.system("git remote add origin https://github.com/%s/%s.git" % (user, repo))    
+
+def render(ifn, ofn, user, repo):
+
+    ifp = open(ifn)
+    ofp = open(ofn, "w")
+    
+    for line in ifp:
+        buf = line
+
+        buf = buf.replace("%user%", user)
+
+        buf = buf.replace("%repo%", repo)
+        buf = buf.replace("%_repo_%", repo.replace("-","_"))
+        buf = buf.replace("%-repo-%", repo.replace("_","-"))
+
+        ofp.write(buf)
+
+    ifp.close()
+    ofp.close()
+    
+if __name__ == "__main__":
+    main()
